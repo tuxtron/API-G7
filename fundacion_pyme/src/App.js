0fd1dc7dd89d79e24f5abc0063cf7a89922a8585
YaoKevinn <vasty85817@gmail.com>
2020-09-11 20:39:51 -0300

    inicio 2

--- fundacion_pyme/src/App.js
+++ fundacion_pyme/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Pregunta from './Components/Pregunta';
 
 
 function App() {
@@ -10,13 +11,13 @@ function App() {
       <div className="App">
           <Switch>
             <Route path="/login"> 
-
+            
             </Route>
             <Route path="/detalle">
 
             </Route>
             <Route path="/">
-
+                <Pregunta />
             </Route>
           </Switch>
       </div>
