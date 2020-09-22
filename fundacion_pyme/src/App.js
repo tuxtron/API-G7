fa56fb5df70048e98468f63fa1a12332817d09f4
YaoKevinn <vasty85817@gmail.com>
2020-09-22 20:33:56 -0300

    ajuste

--- fundacion_pyme/src/App.js
+++ fundacion_pyme/src/App.js
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-import { Button } from 'reactstrap';
->>>>>>> 0aa616431c644b2c0447320d596463d6ea1e9d76
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Pregunta from './Components/Pregunta';
 import ModalEjemplo from './Components/Modal';
 import listaDePreguntas from './listaDePreguntas.json';
-=======
-import "./styles.css";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Pregunta from './Components/Pregunta';
+// import "./styles.css";
 import Login from './Components/login'
->>>>>>> Stashed changes
+import Tabla from './Components/Tabla'
 
 function App() {
 
@@ -26,31 +18,23 @@ function App() {
   return (
     <Router>
           <div className="App">
-              <Login />
           <Switch>
-            <Route path="/login"> 
-            
-<<<<<<< Updated upstream
-            </Route>
-            <Route path="/home"> 
-                <Tabla/>
-            </Route>
-=======
-                  </Route>
->>>>>>> Stashed changes
-            <Route path="/detalle">
-               {
-                  listaDePreguntas.map((pregunta, index) => {
-                        return  <Pregunta 
-                                  key={index}
-                                  objPregunta={pregunta}
-                                  isSupervisor={state.isSupervisor} />
-                  })
+              <Route path="/home"> 
+                  <Tabla/>
+              </Route>
+              <Route path="/detalle">
+                {
+                    listaDePreguntas.map((pregunta, index) => {
+                          return  <Pregunta 
+                                    key={index}
+                                    objPregunta={pregunta}
+                                    isSupervisor={state.isSupervisor} />
+                    })
                 }
-            </Route>
-            <Route path="/">
-              <ModalEjemplo />
-            </Route>
+              </Route>
+              <Route path="/"> 
+                 <Login />
+              </Route>
           </Switch>
       </div>
     </Router>
