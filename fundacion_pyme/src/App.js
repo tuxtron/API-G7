b812196a7422668f3460026b7fe91de1e67b4cb0
AgustinTara <AgustinTara@users.noreply.github.com>
2020-09-22 15:24:19 +1000

    Update App.js

--- fundacion_pyme/src/App.js
+++ fundacion_pyme/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+<<<<<<< Updated upstream
 import { Button } from 'reactstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Pregunta from './Components/Pregunta';
@@ -8,6 +9,12 @@ import ModalEjemplo from './Components/Modal';
 =======
 import Tabla from './Components/Tabla';
 import listaDePreguntas from './listaDePreguntas.json';
+=======
+import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Pregunta from './Components/Pregunta';
+import Login from './Components/login'
+>>>>>>> Stashed changes
 
 >>>>>>> 2af45cf129011d890d4876983a54548835736fe6
 
@@ -15,14 +22,19 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
+          <div className="App">
+              <Login />
           <Switch>
             <Route path="/login"> 
             
+<<<<<<< Updated upstream
             </Route>
             <Route path="/home"> 
                 <Tabla/>
             </Route>
+=======
+                  </Route>
+>>>>>>> Stashed changes
             <Route path="/detalle">
                {
                   listaDePreguntas.map((pregunta, index) => {
